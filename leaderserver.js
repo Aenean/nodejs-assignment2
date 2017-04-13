8ade02cc2fbe8f6f947e5edefcf534f396f5df83
Alper <mrsecret@windowslive.com>
2017-04-13 20:41:20 +0300

    Nodejs Assignment 2

---
+++ leaderserver.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose'),
+    assert = require('assert');
+
+var Leaders = require('./models/leader');
+
+var url = 'mongodb://localhost:27017/conFusion';mongoose.connect(url);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+   console.log('Connected');
+    
+    Leaders.create ({
+        name: 'Peter Pan',
+        image: 'images/alberto.png',
+        designation: 'Chief Epicurious Officer',
+        abbr: 'CEO',
+        description: 'Our CEO, Peter,...'
+    }, function (err, leader) {
+        if (err) throw err;
+        console.log('Leader created!');
+        console.log(leader);
+
+        var id = leader._id;
+
+        // get all the dishes
+        setTimeout(function () {
+            Leaders.findByIdAndUpdate(id, {
+                    $set: {
+                        description: 'Updated Test'
+                    }
+                }, {
+                    new: true
+                })
+                .exec(function (err, leader) {
+                    if (err) throw err;
+                    console.log('Updated Leader!');
+                    console.log(leader);
+
+                        db.collection('leader').drop(function () {
+                            db.close();
+                        });
+                });
+        }, 3000);
+    })
+    
+});
